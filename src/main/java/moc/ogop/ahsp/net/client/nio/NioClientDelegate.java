ed89d91d3990280a7ef2e1c4649c9abbb68aecc8
hippoz <hippozheng@163.com>
2016-07-19 18:15:05 +0800

    Add new demo class

--- src/main/java/moc/ogop/ahsp/net/client/nio/NioClientDelegate.java
+++ src/main/java/moc/ogop/ahsp/net/client/nio/NioClientDelegate.java
@@ -2,8 +2,6 @@ package moc.ogop.ahsp.net.client.nio;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -13,8 +11,6 @@ import moc.ogop.ahsp.net.io.RPacket;
 import java.io.IOException;
 import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Heisenberg on 2016/7/18.
@@ -23,37 +19,20 @@ public class NioClientDelegate<I> extends AbstractClientDelegate {
 
     private final static EventLoopGroup nioEventLoop = new NioEventLoopGroup(10);
 
-    private final CompletableFuture<Channel> channelFuture = new CompletableFuture<>();
+    private final static Bootstrap clientBootStrap = new Bootstrap();
+
+    private final Channel channel;
 
     public NioClientDelegate(String hostName, int portNumber, Class<I> serviceInterface) throws IOException, InterruptedException {
         super(serviceInterface);
-        Bootstrap b = new Bootstrap();
-        b.group(nioEventLoop)
+        clientBootStrap.group(nioEventLoop)
                 .channel(NioSocketChannel.class)
                 .handler(new NioClientChannelInitializer(pending));
-        ChannelFuture f = b.connect(new InetSocketAddress(hostName, portNumber));
-        f.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture cf) throws Exception {
-                if (cf.isSuccess()) {
-                    System.out.println("Connected");
-                    channelFuture.complete(cf.channel());
-                } else {
-                    System.err.println("Connection attempt failed");
-                    cf.cause().printStackTrace();
-                }
-            }
-        });
+        channel = clientBootStrap.connect(new InetSocketAddress(hostName, portNumber)).sync().channel();
     }
 
     protected void sendToServer(RPacket rp) throws IOException {
-        try {
-            channelFuture.get().writeAndFlush(rp);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        channel.writeAndFlush(rp);
     }
 
     public static <I> I remoteService(String hostName, int portNumber, Class<I> serviceInterface) throws IOException, InterruptedException {
